211caeabbc668768116a13598fa939c202fc56c0
rimuzakki <rimuzakki@gmail.com>
2019-10-09 06:47:08 +0700

    update

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,35 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Form extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
       text: ''
     }
-  }  
+  }
 
   onChangeHandler = (e) => {
-    this.setState({ text : e.target.value })
+    this.setState({ text: e.target.value })
   }
 
   onSubmit = () => {
-    this.props.submit(this.state.text)
+    // this.props.submit(this.state.text)
+    this.props.addTodos(this.state.text)
     this.setState({ text: '' })
   }
 
-  render () {
+  render() {
     return (
       <>
         <p>Add Todo</p>
         <div className="form-group">
-          <input 
-          value={this.state.text}
-          onChange={this.onChangeHandler} type="text" className="form-control" placeholder="Type todo here" />
+          <input
+            value={this.state.text}
+            onChange={this.onChangeHandler} type="text" className="form-control" placeholder="Type todo here" />
         </div>
         <button onClick={this.onSubmit} className="btn btn-primary">Add</button>
       </>
     )
   }
-} 
+}
 
-export default Form
+const mapDispatchToProps = dispatch => {
+  return {
+    addTodos: (todo) => dispatch({ type: 'ADD_TODOS', payload: todo })
+  }
+}
+export default connect(null, mapDispatchToProps)(Form)
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,16 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class List extends Component {
-
-  render (){
+  render() {
     return (
       <ul className="list-group">
         {this.props.todos.map(todo => (
           <li className="list-group-item">{todo}</li>
         ))}
       </ul>
-    )    
+    );
   }
 }
-
-export default List
+const mapStateToProps = state => {
+  return {
+    todos: state.todos
+  };
+};
+export default connect(mapStateToProps, null)(List);
